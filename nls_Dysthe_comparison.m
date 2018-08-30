3d6c037e052e356ecde13e1b0ee4892bb241d69a
Chris Curtis <ccurtis@mail.sdsu.edu>
2018-08-30 11:20:51 -0700

    Getting this going

---
+++ nls_Dysthe_comparison.m
@@ -0,0 +1,67 @@
+function nls_Dysthe_comparison(Llx,K,ep,rms,tf,dt,om,sig,width,Nens)
+    
+    % [-Llx,Llx] is size of simulation
+    % K is number of modes in simulation
+    % ep is magnitude of perturbation in Dysthe equation
+    % tf is length of time to let simulation run
+    % dt is time step in RK4 solver for time simulation
+    % k0 is wave number of carrier wave 
+    % om is magnitude of vorticity 
+    % sig is surface tension
+    % Nens is number of ensemble members
+
+    disp('Spatial mesh size is')
+    disp(Llx/K)
+    
+    nmax = round(tf/dt); % Step size for time integrator and number of time steps
+    k0 = pi*ep/Llx*floor(Llx/(pi*ep));
+    disp('Carrier wave number is')
+    disp(k0)
+    
+    [Om,cg,ad,anl] = param_maker(k0,om,sig);    
+    
+    KT = 2*K;
+    
+    Kc = floor(KT/3);
+    Kuc = KT - Kc + 1;
+    Kc = Kc + 1;    
+    
+    Kvec = [ 0:K -K+1:-1 ]';
+    Kmesh = pi/Llx*Kvec;
+        
+    Xmesh = -Llx:Llx/K:Llx-Llx/K;
+    
+    uints = zeros(KT,Nens);
+    nls_sol = zeros(KT,Nens);
+    dysthe_sol = zeros(KT,Nens);
+    
+    rvec = exp(-(Kmesh).^2/(2*width^2));
+    
+    for jj=1:Nens
+        arand = 2*pi*1i*rand(KT,1);
+        uints(:,jj) = KT*rms*sqrt(sqrt(pi)/(Llx*width))*rvec.*exp(arand);    
+        uints(Kc:Kuc,jj) = 0;
+        %plot(Xmesh,abs(ifft(uints(:,jj))),'k','LineWidth',2)
+        %pause
+    end
+    
+    aval = rms*sqrt(sqrt(pi)/(Llx*width)*sum(rvec.^2));
+    disp('Root mean square NLS amplitude')
+    disp(aval)
+    disp('Minimal Stable Envelope Width')
+    disp(4*aval)
+    
+    parfor jj=1:Nens
+        nls_sol(:,jj) = nls_solver(K,Llx,nmax,ad,anl,dt,uints(:,jj));
+        dysthe_sol(:,jj) = vor_Dysthe_solver(K,Llx,nmax,ad,anl,cg,k0,Om,om,sig,ep,dt,uints(:,jj));
+    end
+    
+    nls_spec_mean = fftshift(abs(mean(nls_sol.*conj(nls_sol),2))/KT^2);
+    dysthe_spec_mean = fftshift(abs(mean(dysthe_sol.*conj(dysthe_sol),2))/KT^2);
+    mean_int = fftshift(abs(mean(uints.*conj(uints),2))/KT^2);
+    
+    kvec = pi/Llx*(-K+1:K)';
+    
+    plot(kvec,mean_int,'k-',kvec,nls_spec_mean,'k--',kvec,dysthe_spec_mean,'k-.','LineWidth',2)
+end
+
---
+++ vor_Dysthe_solver.m
@@ -0,0 +1,51 @@
+function w = vor_Dysthe_solver(K,Llx,nmax,ad,anl,cg,k0,Om,om,sig,ep,dt,uint)
+
+    % solves u_t = dk2Om*u_xx + a3*|u|^2*u 
+
+    % Llx is domain size, i.e. solve on domain [-Llx,Llx]
+    % sig is -1 for bright/focusing, +1 for dark/defocusing
+    % tf is total simulation time
+    % mu is magnitude of damping on envelope of dark initial condition
+    Kvec = [ 0:K -K+1:-1 ]';
+    Kmesh = pi/Llx*Kvec;    
+    Dx = 1i*Kmesh;
+    Dx2 = Dx.^2;
+    s = sign(k0);
+    %L0i = (1 + 2*1i*ep*s*cg/(om-2*s*Om)*Dx).^(-1);
+    L0i = (1 - 2*1i*ep*s*cg/(om-2*s*Om)*Dx);
+    %Lap = dt*L0i.*(1i*ad + ep*sig/(om-2*s*Om)*Dx).*Dx2;
+    Lap = dt*(L0i*1i*ad+ ep*sig/(om-2*s*Om)*Dx).*Dx2;
+    KT = 2*K;
+    Kc = floor(KT/3);
+    Kuc = KT - Kc + 1;
+    Kc = Kc + 1;
+    
+    % Bright/Focusing initial conditions       
+    w = uint; 
+        
+    % Begin setup of RK4 method
+    E = exp(Lap/2);
+    E2 = E.^2;
+                
+    % Solve NLS equation in time    
+    for nn=1:nmax            
+        a = dt*nonlinearity(w,k0,cg,om,Om,ep,sig,ad,anl,Dx,L0i);
+        af = E.*(w+a/2);
+        af(Kc:Kuc) = 0;
+        
+        b = dt*nonlinearity(af,k0,cg,om,Om,ep,sig,ad,anl,Dx,L0i);
+        bf = E.*w + b/2;
+        bf(Kc:Kuc) = 0;
+        
+        c = dt*nonlinearity(bf,k0,cg,om,Om,ep,sig,ad,anl,Dx,L0i);
+        cf = E2.*w + E.*c;
+        cf(Kc:Kuc) = 0;
+        
+        d = dt*nonlinearity(cf,k0,cg,om,Om,ep,sig,ad,anl,Dx,L0i);
+        d(Kc:Kuc) = 0;
+        
+        w = E2.*w + (E2.*a + 2*E.*(b+c) + d)/6;    
+        w(Kc:Kuc) = 0;
+    end
+    
+end
